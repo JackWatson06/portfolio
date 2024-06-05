4bd603c4707f47af28458468e8ce0569a5389d28
Jack Watson <watson.jack.p@gmail.com>
2024-06-04 18:19:36 -0600

    Cleaned up top nav

--- app/NavigationLinks.tsx
+++ app/NavigationLinks.tsx
@@ -44,25 +44,25 @@ const navigation_links: Array<NavigationLink> = [
 
 export default function NavigationOptions() {
   const pathname = usePathname();
-  const routed_navigation_links = createRoutedNavigationOptions(navigation_links, pathname);
+  const routed_navigation_links = createRoutedNavigationOptions(
+    navigation_links,
+    pathname,
+  );
 
   return (
     <div className="border-slate-30 h-9 border-b px-8 text-base md:mx-auto md:flex md:h-full md:max-w-96 md:flex-col md:justify-center md:border-none md:px-0 xl:text-lg">
       <div className="flex h-full items-end text-center md:h-auto">
         {routed_navigation_links.map((routed_navigation_link) => {
           return (
-            <Link key={routed_navigation_link.link} href={routed_navigation_link.link} className={`basis-1/3 ${routed_navigation_link.active && "text-lime-800"}`}>
-              {routed_navigation_link.name}
-            </Link>
-          );
-        })}
-      </div>
-      <div className="flex items-end text-center">
-        {routed_navigation_links.map((routed_navigation_link) => {
-          return (
-            <div key={routed_navigation_link.link} className="flex basis-1/3 justify-center">
+            <div key={routed_navigation_link.link} className="basis-1/3">
+              <Link
+                href={routed_navigation_link.link}
+                className={`${routed_navigation_link.active && "text-lime-800"}`}
+              >
+                {routed_navigation_link.name}
+              </Link>
               {routed_navigation_link.active && (
-                <div className="w-20 border-b border-lime-800 md:border-b-2 xl:w-24"></div>
+                <div className="w-20 border-b m-auto border-lime-800 md:border-b-2 xl:w-24"></div>
               )}
             </div>
           );
---
+++ app/resume/page.tsx
@@ -0,0 +1,7 @@
+export default function Resume() {
+  return (
+    <>
+      <p>Resume Page!</p>
+    </>
+  );
+}
